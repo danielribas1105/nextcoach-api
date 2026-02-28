13e9be49a9e457157092056bb6b3e185c810edbc
daniel.ribas <daniel.ribas@isdbflowtech.com>
2026-02-28 13:27:24 -0300

    chore: create db and tables

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ import {
 } from "fastify-type-provider-zod"
 import z from "zod"
 
+import { auth } from "./lib/auth.js"
+
 const app = Fastify({
 	logger: true,
 })
@@ -19,24 +21,24 @@ const app = Fastify({
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
-app.register(fastifySwagger, {
+await app.register(fastifySwagger, {
 	openapi: {
 		info: {
-			title: "NextCoach AI",
-			description: "API NextCoach AI app",
+			title: "NextCoach API",
+			description: "API para app NextCoach AI",
 			version: "0.0.1",
 		},
 		servers: [
 			{
 				description: "Localhost",
-				url: "http://localhost/3000",
+				url: "http://localhost:3000",
 			},
 		],
 	},
 	transform: jsonSchemaTransform,
 })
 
-app.register(fastifySwaggerUI, {
+await app.register(fastifySwaggerUI, {
 	routePrefix: "/docs",
 })
 
@@ -60,9 +62,45 @@ app.withTypeProvider<ZodTypeProvider>().route({
 	},
 })
 
+app.route({
+	method: ["GET", "POST"],
+	url: "/api/auth/*",
+	async handler(request, reply) {
+		try {
+			// Construct request URL
+			const url = new URL(request.url, `http://${request.headers.host}`)
+
+			// Convert Fastify headers to standard Headers object
+			const headers = new Headers()
+			Object.entries(request.headers).forEach(([key, value]) => {
+				if (value) headers.append(key, value.toString())
+			})
+			// Create Fetch API-compatible request
+			const req = new Request(url.toString(), {
+				method: request.method,
+				headers,
+				...(request.body ? { body: JSON.stringify(request.body) } : {}),
+			})
+			// Process authentication request
+			const response = await auth.handler(req)
+			// Forward response to client
+			reply.status(response.status)
+			response.headers.forEach((value, key) => reply.header(key, value))
+			reply.send(response.body ? await response.text() : null)
+		} catch (error) {
+			/* app.log.error("Authentication Error:", error) */
+			app.log.error(error)
+			reply.status(500).send({
+				error: "Internal authentication error",
+				code: "AUTH_FAILURE",
+			})
+		}
+	},
+})
+
 // Run the server!
 try {
-	await app.listen({ port: Number(process.env.PORT || 8081) })
+	await app.listen({ port: Number(process.env.PORT) || 8081 })
 } catch (err) {
 	app.log.error(err)
 	process.exit(1)
